5389d44cf41227012158cce8de2acdd714d66e7f
César Ramón Canto Escobar <caesar_caento@hotmail.com>
2019-12-27 15:02:18 -0600

    update

---
+++ KisinGame/Assets/SetActiveSceneAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetActiveSceneAction: MonoBehaviour
+{
+    [SerializeField] int sceneToActivate;
+
+    //Method Called inside GameManager GameManager FSM
+    public void SetActiveScene(int sceneToActivate)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneToActivate));
+    }
+}
