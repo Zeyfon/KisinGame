b19c1db2fd6aecd806639a502e7eaac7400bc695
César Ramón Canto Escobar <caesar_caento@hotmail.com>
2020-03-18 12:49:58 -0600

    Dialogues Update V0.1
    Dialogue prefab was udpated and scene 1 and 2 dialogues were updated

--- KisinGame/Assets/Scripts/Dialogues/ConversationStarter.cs
+++ KisinGame/Assets/Scripts/Dialogues/ConversationStarter.cs
@@ -5,26 +5,10 @@ using PixelCrushers.DialogueSystem;
 
 public class ConversationStarter : MonoBehaviour
 {
-    [SerializeField] BossRoomController bossRoomController;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            //The collider is disabled to stop the dialogue to be activated again
-            GetComponent<Collider2D>().enabled = false;
-            //collision.GetComponent<PlayerIdentifer>().GetDialogueReference(transform);
-        }
-
-    }
 
     public void StartConversation()
     {
         GetComponent<DialogueSystemTrigger>().OnUse();
     }
 
-    public void StartBossFight()
-    {
-        bossRoomController.StartFight();
-    }
 }
