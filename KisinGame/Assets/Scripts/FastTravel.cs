5151e1b78884382b9040a2c4fc3a37912ae9b91a
César Ramón Canto Escobar <caesar_caento@hotmail.com>
2020-03-29 18:26:02 -0600

    General Update Fix V0.6
    Dialogues Update to allow the Conversants to change properly between sprites.
    Cheats Menu Added
    Boss Animations Updated

---
+++ KisinGame/Assets/Scripts/FastTravel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class FastTravel : MonoBehaviour
+{
+    Transform gameManager;
+    PlayMakerFSM[] pFSMs;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.3f);
+        while (!gameManager)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void FasTravel(int sceneBuildIndex)
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").transform;
+        pFSMs = gameManager.GetComponents<PlayMakerFSM>();
+        foreach (PlayMakerFSM fsm in pFSMs)
+        {
+            if (fsm.FsmName == "GameManager")
+            {
+                FsmEventData myfsmEventData = new FsmEventData();
+                myfsmEventData.IntData = sceneBuildIndex;
+                HutongGames.PlayMaker.Fsm.EventData = myfsmEventData;
+                fsm.SendEvent("FastTravel");
+            }
+        }
+        ES3.DeleteFile("SaveData");
+    }
+}
