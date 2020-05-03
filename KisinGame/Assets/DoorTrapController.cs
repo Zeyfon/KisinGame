7837712543bf8da6e7d6fb331ba1ca5c7ce965cb
César Ramón Canto Escobar <caesar_caento@hotmail.com>
2020-05-03 09:29:36 -0500

    Beta Update V0.4

---
+++ KisinGame/Assets/DoorTrapController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrapController : MonoBehaviour
+{
+    [SerializeField] string keyName = null;
+    [SerializeField] Transform trapDoor1Transform = null;
+    [SerializeField] Transform trapDoor2Transform = null;
+
+    [SerializeField] AudioClip doorSound = null;
+
+    Door trap1Door;
+    Door trap2Door;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        yield return new WaitForSeconds(0.3f);
+        trap1Door = trapDoor1Transform.GetComponent<Door>();
+        trap2Door = trapDoor2Transform.GetComponent<Door>();
+        OpenedDoorState();
+        if (ES3.KeyExists(keyName))
+        {
+            GetComponent<Collider2D>().enabled = false;
+            yield break;
+        }
+
+    }
+
+    void OpenedDoorState()
+    {
+        trap1Door.OpenedState();
+        trap2Door.OpenedState();
+    }
+
+    void CloseDoors()
+    {
+        audioSource.PlayOneShot(doorSound);
+        trap1Door.CloseDoor();
+        trap2Door.CloseDoor();
+    }
+
+    void OpenDoors()
+    {
+        audioSource.PlayOneShot(doorSound);
+        trap1Door.OpenDoor();
+        trap2Door.OpenDoor();
+    }
+
+    public void DisableTrap()
+    {
+        ES3.Save<bool>(keyName, true);
+        OpenDoors();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            CloseDoors();
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+
+}
--- KisinGame/Assets/Scripts/Enemies/Level3_Boss/CrystalRainSlot.cs
+++ KisinGame/Assets/Scripts/Enemies/Level3_Boss/CrystalRainSlot.cs
@@ -8,8 +8,8 @@ public class CrystalRainSlot : MonoBehaviour
     [SerializeField] float timeToDisable;
 
     [Header("Movement Limits")]
-    [SerializeField] Transform upperLimit;
-    [SerializeField] Transform lowerLimit;
+    [SerializeField] Transform initialTransform;
+    [SerializeField] Transform finalTransform;
 
     [Header("Timing Values")]
     [Range(0,0.2f)]
@@ -27,25 +27,33 @@ public class CrystalRainSlot : MonoBehaviour
         StartCoroutine(MoveRainSlot(crystalRainAttack));
     }
 
+    public void SetInitialPosition()
+    {
+        transform.position = new Vector3(transform.position.x, initialTransform.position.y, 0);
+    }
+
+
+
     IEnumerator MoveRainSlot(CrystalRainAttack crystalRainAttack)
     {
         float timer = 0;
-        Vector3 upperLimitPosition = upperLimit.position;
-        Vector3 lowerLimitPosition = lowerLimit.position;
-        while (transform.position.y <= (upperLimitPosition.y - .05f))
+        Vector3 initialPosition = this.initialTransform.position;
+        Vector3 finalPosition = new Vector3(transform.position.x, transform.position.y+1f,0);
+        while (transform.position.y <= (finalPosition.y - .05f))
         {
             timer += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, upperLimitPosition, upwardLerp);
+            transform.position = Vector3.Lerp(transform.position, finalPosition, upwardLerp);
             //print("Moving Upwards");
             yield return null;
         }
         yield return new WaitForSeconds(timeBetweenUpDown);
         print("falling");
         GetComponent<AudioSource>().PlayOneShot(slideDownSound, 1);
-        Vector3 initialPosition = transform.position;
-        while (transform.position.y >= (lowerLimitPosition.y + .05f))
+        initialPosition = transform.position;
+        finalPosition = new Vector3(transform.position.x, finalTransform.position.y, 0);
+        while (transform.position.y >= (finalPosition.y + .05f))
         {
-            transform.position = Vector3.Lerp(transform.position, lowerLimitPosition, downwardLerp);
+            transform.position = Vector3.Lerp(transform.position, finalPosition, downwardLerp);
             //print("Moving Downwards");
             yield return null;
         }
